8905ec627f20803fa541e6ee5a640e7e162ac096
celsobomfim28 <lufan20@gmail.com>
2021-10-09 02:08:57 -0300

    up project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.project_doc {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    //requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires javafx.graphics;
+
+    opens com.example.project_doc to javafx.fxml;
+    exports com.example.project_doc;
+}
